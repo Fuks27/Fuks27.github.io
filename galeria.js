ea52f3c1754a61760f92d45a90a86abd7aed4858
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-12 15:02:37 +0100

    Update galeria.js

--- galeria.js
+++ galeria.js
@@ -40,19 +40,15 @@ function showSlides(n) {
         closeModal();
     }
 }
-
-
 document.onkeydown = function(evt) {
   evt = evt || window.event;
   if (evt.keyCode == 37) {
       plusSlides(-1);
   }
 }
-
 document.onkeydown = function(evt) {
   evt = evt || window.event;
   if (evt.keyCode == 39) {
       plusSlides(1);
   }
 }
-{}
