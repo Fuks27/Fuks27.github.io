54e03b1a1236f0fc06637193c1a0faa2315a4f5c
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-12 14:53:09 +0100

    Update galeria2.js

--- galeria2.js
+++ galeria2.js
@@ -1,57 +1,42 @@
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openModal2() {
+  document.getElementById("myModal2").style.display = "block";
 }
 
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModal2() {
+  document.getElementById("myModal2").style.display = "none";
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides2(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides2(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  var captionText = document.getElementById("caption2");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active2", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active2";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
   document.onkeydown = function(evt) {
     evt = evt || window.event;
     if (evt.keyCode == 27) {
-        closeModal();
+        closeModal2();
     }
-}
-
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event;
-  if (evt.keyCode == 37) {
-      plusSlides(-1);
-  }
-}
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event;
-  if (evt.keyCode == 39) {
-      plusSlides(1);
-  }
 }
