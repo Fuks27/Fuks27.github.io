d8d934945fb9d2b97380c624a7feec58158b14ce
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-14 03:07:59 +0100

    a

---
+++ Javascript/week-meno.js
@@ -0,0 +1,12 @@
+function findOddEven(){
+    //get the input value
+    var num = document.getElementById("demo").innerHTML = ( + new Date().getWeekNumber());
+
+    //if the value of c is 2 then it is a prime number
+    //because a prime number should be exactly divisible by 2 times only (itself and 1)
+    if ( num % 2 == 0) {
+        document.getElementById('result').innerHTML = num + ' is a Even number';
+    }else{
+        document.getElementById('result').innerHTML = num + ' is a Odd number';
+    }
+}       
