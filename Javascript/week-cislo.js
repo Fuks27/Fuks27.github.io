24bce8d6f1925cb402960a7174c61751606abdfc
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-14 03:39:14 +0100

    a

--- Javascript/week-cislo.js
+++ Javascript/week-cislo.js
@@ -5,8 +5,8 @@ function findOddEven(){
     //if the value of c is 2 then it is a prime number
     //because a prime number should be exactly divisible by 2 times only (itself and 1)
     if ( num % 2 == 0) {
-        document.getElementById('result').innerHTML = 'Nepárny týždeň' ;
+        document.getElementById('result').innerHTML = 'Párny týždeň' ;
     }else{
-        document.getElementById('result').innerHTML ='Párny týždeň';
+        document.getElementById('result').innerHTML ='Nepárny týždeň';
     }
 }
