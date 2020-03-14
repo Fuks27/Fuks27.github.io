ad2a866409caa0236011b7e42b0acfbe73155988
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-14 03:46:25 +0100

    fuha

--- Javascript/date.js
+++ Javascript/date.js
@@ -3,5 +3,5 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+today = dd + '.' + mm + '.' + yyyy;
+  document.getElementById('vysledok').innerHTML = (today);
